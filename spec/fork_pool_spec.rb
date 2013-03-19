b2deaea7479d92cf67d7ae48b928f622600f73fe
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2013-03-19 23:54:07 +0100

    stupid tabs again
    this is the last time I will try to fix it.

--- spec/fork_pool_spec.rb
+++ spec/fork_pool_spec.rb
@@ -10,33 +10,33 @@ describe ForkPool do
   end
 
   describe "#empty?" do
-  	subject { ForkPool }
-  	it("2 in the pool should not feel empty") { subject.new(2).empty?.should be_false }
-  	it("1 in the pool is lonely but not empty") { subject.new(1).empty?.should be_false }
+    subject { ForkPool }
+    it("2 in the pool should not feel empty") { subject.new(2).empty?.should be_false }
+    it("1 in the pool is lonely but not empty") { subject.new(1).empty?.should be_false }
 
     it("none in the pool is empty") do
-    	pending "the constructor won't allow this"
-    	subject.new(0).empty?.should be_true 
+      pending "the constructor won't allow this"
+      subject.new(0).empty?.should be_true 
     end
 
     it("negative numbers in the pool? are these forks made of antimatter") do
-    	pending "of course, the constructor complains about the negative number"
-    	subject.new(-1).empty?.should be_true
+      pending "of course, the constructor complains about the negative number"
+      subject.new(-1).empty?.should be_true
     end
   end
 
   describe "#pool" do
-  	subject { ForkPool.new(3) }
-  	its(:pool) { should == 3 }
+    subject { ForkPool.new(3) }
+    its(:pool) { should == 3 }
   end
 
   describe "#spork" do
-  	subject { ForkPool.new(2) }
-  	pending("right... how does one test such a thing?")
+    subject { ForkPool.new(2) }
+    pending("right... how does one test such a thing?")
   end
 
   describe "#wait" do
-  	subject { ForkPool.new(2) }
-  	pending("ehm...?")
+    subject { ForkPool.new(2) }
+    pending("ehm...?")
   end
 end
